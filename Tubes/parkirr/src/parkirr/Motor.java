e13bc3ac919a5ca22d0f2f3de49b126ac9bb6266
rendikrwn <107618063+rendikrwn@users.noreply.github.com>
2022-06-17 00:47:49 +0800

    Add files via upload

---
+++ Tubes/parkirr/src/parkirr/Motor.java
@@ -0,0 +1,19 @@
+
+package parkirr;
+
+public class Motor extends Kendaraan {
+    int biayaParkirMotor = 3000;
+   
+    public Motor(String platNomor, int biayaParkirMotor) {
+        super(platNomor);
+        this.biayaParkirMotor = biayaParkirMotor;
+    }
+
+    public void setBiayaParkirMotor(int biayaParkirMotor) {
+        this.biayaParkirMotor = biayaParkirMotor;
+    }
+    
+    public int getBiayaParkirMotor() {
+        return biayaParkirMotor;
+    }   
+}
